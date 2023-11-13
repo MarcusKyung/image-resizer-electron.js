1dfbbfe59d55218f00f228750a3f9fae72865f03
MarcusKyung <Kyungmj@gmail.com>
2023-11-12 16:57:38 -0800

    add: completed image resizer

--- main.js
+++ main.js
@@ -1,12 +1,18 @@
 const path = require('path');
-const { app, BrowserWindow, Menu } = require('electron');
+const { app, BrowserWindow, Menu, ipcMain, shell } = require('electron');
+const os = require('os');
+const fs = require('fs');
+const resizeImg = require('resize-img');
+
+process.env.NODE_ENV = 'production';
 
 const isMac = process.platform === 'darwin';
-const isDev = process.env.NODE_ENV !== 'development';
+const isDev = process.env.NODE_ENV !== 'production';
 
+let mainWindow;
 
 function createMainWindow() {
-  const mainWindow = new BrowserWindow({
+  mainWindow = new BrowserWindow({
     title: 'Image Resizer',
     width: isDev ? 1000: 500,
     height: 600,
@@ -30,7 +36,9 @@ function createAboutWindow() {
   const aboutWindow = new BrowserWindow({
     title: 'About Window',
     width: 300,
-    height: 300
+    height: 300,
+    title: 'About Image Resizer',
+    icon: `${__dirname}/assets/icons/Icon_256x256.png`,
   })
 
   aboutWindow.loadFile(path.join(__dirname, './renderer/about.html'))
@@ -43,6 +51,9 @@ app.whenReady().then(() => {
   // Implements Menu:
   const mainMenu = Menu.buildFromTemplate(menu);
   Menu.setApplicationMenu(mainMenu);
+
+  //Remove main window from memory on close
+  mainWindow.on('closed', () => mainWindow = null);
   
   app.on('activate', () => {
     if (BrowserWindow.getAllWindows().length === 0) {
@@ -76,6 +87,38 @@ const menu = [
     ]
   }] : [])
 ];
+
+// Respond to ipcRender resize:
+ipcMain.on('image:resize', (e, options) => {
+  options.dest = path.join(os.homedir(), 'imageresizer');
+  resizeImage(options);
+})
+
+async function resizeImage({imgPath, width, height, dest}) {
+  try {
+    const newPath = await resizeImg(fs.readFileSync(imgPath), {
+      width: +width,
+      height: +height
+    })
+
+    const filename = path.basename(imgPath);
+
+    // Create dest folder if it doesn't exist
+    if (!fs.existsSync(dest)) {
+      fs.mkdirSync(dest);
+    }
+
+    //Write file to dest
+    fs.writeFileSync(path.join(dest, filename), newPath);
+
+    // Send success message to renderer
+    mainWindow.webContents.send('image:done');
+    // Open Dest Folder
+    shell.openPath(dest);
+  } catch (error) {
+    console.log(error);
+  }
+}
 
 // Boilerplate: Quit when all windows are closed, needed to close program on Windows
 app.on('window-all-closed', () => {
--- renderer/js/renderer.js
+++ renderer/js/renderer.js
@@ -10,7 +10,7 @@ function loadImage(e) {
   const file = e.target.files[0];
 
   if(!isFileImage(file)) {
-    console.log('Please select an image');
+    alertError("message: File is not an image.")
     return;
   }
 
@@ -25,13 +25,69 @@ function loadImage(e) {
   form.style.display = 'block';
   filename.innerText = file.name;
   outputPath.innerText = path.join(os.homedir(), 'imageresizer')
-
 }
+
+//Sends image data to main process
+function sendImage(e) {
+  e.preventDefault();
+
+  const imgPath = img.files[0].path;
+  const width = widthInput.value;
+  const height= heightInput.value;
+  
+  if (!img.files[0]) {
+    alertError("Please upload an image");
+    return;
+  }
+
+  if (width === '' || height === '') {
+    alertError("Please enter a valid width and height");
+    return;
+  }
+
+  ipcRenderer.send("image:resize", {
+    imgPath,
+    width,
+    height
+  });
+}
+
+// Catch the image:done event 
+ipcRenderer.on('image:done', () => {
+  alertSuccess(`Image resized to ${widthInput.value} x ${heightInput.value}`);
+})
 
 function isFileImage(file) {
   const acceptedImageTypes = ['image/gif', 'image/png', 'image/jpeg'];
   return file && acceptedImageTypes.includes(file['type']);
 }
 
+function alertError(message) {
+  Toastify.toast({
+    text: message,
+    duration: 5000,
+    close: false,
+    style: {
+      background: "red",
+      color: "white",
+      textAlign: "center",
+    },
+  })
+}
+
+function alertSuccess(message) {
+  Toastify.toast({
+    text: message,
+    duration: 5000,
+    close: false,
+    style: {
+      background: "green",
+      color: "white",
+      textAlign: "center",
+    },
+  })
+}
+
 img.addEventListener('change', loadImage);
+form.addEventListener('submit', sendImage);
 
